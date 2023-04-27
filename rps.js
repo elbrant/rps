d3592a16ec5466d9630fef9fb0ef0ad212befa6d
elBrant <54511367+elbrant@users.noreply.github.com>
2023-04-27 19:07:01 -0400

    Commit project files
    rock paper scissors, completed, for now

--- rps.js
+++ rps.js
@@ -1,122 +1,128 @@
 // TOP Rock Paper scissors exercise
-// by elBrant, 24FEB2023
-
+// author: elBrant, 24FEB2023
+// call-stack: game()-> wwcd()-> tally()-> myWin() or pcWin()-> track()-> gameover() 
 
 // establish global variables 
-let round;
-let myScore; myScore = isNaN(myScore) ? 0 : myScore;
-let pcScore; pcScore = isNaN(pcScore) ? 0 : pcScore; 
 let msg;
+let myPlay;	
+let myScore; 
+let pcScore; 
 let result;
+let round; 
 
+function $(id) { return document.getElementById(id); }
+function get() { return sessionStorage.getItem; }
+function set() { return sessionStorage.setItem; }	
 
-// playerSelection (begin game with onclick event)
 function game(x) {
-	let myPlay = x; 
+	// playerSelection (player onclick event initiates game)
+	if (x === "rock" || "paper" || "scissors") { myPlay = x; } 
+	else { window.location.reload() } //avoid malicious code 
+
+	//getComputerChoice() 
+	let pcRoll = Math.floor(Math.random() * 3) + 1;
 	let pcPlay;
 
-	// getComputerChoice-- works
-	let pcRoll = Math.floor(Math.random() * 3) + 1;
-	if (pcRoll == 1) { pcPlay = "rock"; } 
-	else if (pcRoll == 2) { pcPlay = "paper"; } 
-	else { pcPlay = "scissors"; }
+	if (pcRoll === 1) { pcPlay = "rock"; } 
+	else if (pcRoll === 2) { pcPlay = "paper"; } 
+	else { pcPlay = "scissors"; }	
 
 	// establish HTML output -- works
-	document.getElementById("me").innerHTML = myPlay;
-	document.getElementById("pc").innerHTML = pcPlay;
+	$("me").innerHTML = myPlay;
+	$("pc").innerHTML = pcPlay;
 
-	// winner winner chicken dinner 
 	wwcd(myPlay, pcPlay);
+} // end game
+ 
+function wwcd(myPlay, pcPlay) { 
+	// get and set session scores
+	tally();
 
-} // end game()
+	// clear leftover messages
+	msg = ""; 
+	result = "";
 
+	// winner winner chicken dinner; determine winner; return results
+	if (myPlay === pcPlay) { msg = "tie!"; result = "this round won't count"; }
 
-// winner winner chicken dinner; determine winner; return results
-function wwcd(myPlay, pcPlay) {
-	if (myPlay === pcPlay) { msg = "tie!"; result = "this round won't count";  
 	// rock breaks scissors
-	} else if ((myPlay == "rock" && pcPlay == "scissors") || (myPlay == "scissors" && pcPlay == "rock")) {
-		msg = "rock breaks scissors <br>";
-		if (myPlay == "rock") {	myWin(); } else { pcWin(); }
+	else if (((myPlay === "rock") && (pcPlay === "scissors")) || ((myPlay === "scissors") && (pcPlay === "rock"))) {
+		msg = "rock breaks scissors <br>"; 
+		if (myPlay === "rock") {	myWin(); } else { pcWin(); } }
+
 	// paper covers rock
-	} else if ((myPlay == "paper" && pcPlay == "rock") || (myPlay == "rock" && pcPlay == "paper")) {
-		msg = "paper covers rock";
-		if (myPlay == "paper") { myWin(); } else { pcWin(); }
+	else if (((myPlay === "paper")  && (pcPlay === "rock")) || ((myPlay === "rock") && (pcPlay === "paper"))) {
+		msg = "paper covers rock"; 
+		if (myPlay === "paper") { myWin(); } else { pcWin(); } }
+
 	// scissors cuts up paper
-	} else if (myPlay == "scissors" && pcPlay == "paper") {
-		msg = "scissors cut up paper";
-		myWin();
-	} else {  //(myPlay == "paper" && pcPlay == "scissors")
-		msg = "scissors cut up paper";
-		pcWin();
-	}
-	// establish HTML output -- works
-	document.getElementById("toss").innerHTML = msg;
-	document.getElementById("winner").innerHTML = result; 
+	else { 
+		msg = "scissors cuts up paper"; 
+		if (myPlay === "scissors") { myWin(); } else { pcWin(); } } 
+	
+	// establish HTML output 
+	$("toss").innerHTML = msg;
+	$("winner").innerHTML = result;
 
-}
+	function myWin() { 
+		// player won round
+		myScore += 1;  
+		set("myScore", myScore);
+		track();
 
-// scorekeeper
+		// return msgs
+		if (myScore === 3) {
+			if (myScore === round) { result = " you are my <br> HERO!! <br> it takes skill to skunk the computer, and you did it!"; }
+			else { result = "<span onclick='window.location.reload();'> congratulations!!! <br> winner winner chicken dinner! </span>"; } 
+			gameover(); }
+		else { result = "YOU won round " + round + "! "; } 
+	} 
+
+	function pcWin() {
+		// pc won round
+		pcScore += 1; 
+		set("pcScore", pcScore);
+		track();
+
+		// return msgs
+		if (pcScore === 3) {
+			if (pcScore === round) { result = " don't stop now ...  <br> you'll get the hang of this "; }
+			else { result = "good try! <br> computer wins game"; }
+			gameover(); }
+		else { result = "PC wins round " + round + "! "; } 
+	}	
+} // end wwcd
+
 function tally() {
-	if (!sessionStorage.getItem("myScore")) { 
-		myScore = 0; 
-		myScore = isNaN(myScore) ? 0 : myScore;
-		sessionStorage.setItem("myScore", myScore); 
-	} else { 
-		sessionStorage.getItem("myScore"); 
-		myScore = isNaN(myScore) ? 0 : myScore;
-		sessionStorage.setItem("myScore", myScore); 
-	}
-	console.log(myScore); 
+	// get and set session/scores
+	if (!get("myScore")) { myScore === 0; set("myScore", myScore); } 
+	else { get("myScore"); myScore = isNaN(myScore) ? 0 : myScore; }
 
-	if (!sessionStorage.getItem("pcScore")) {
-		pcScore = 0; 
-		pcScore = isNaN(pcScore) ? 0 : pcScore;
-		sessionStorage.setItem("pcScore", pcScore); 
-	} else { 
-		sessionStorage.getItem("pcScore"); 
-		pcScore = isNaN(pcScore) ? 0 : pcScore;
-		sessionStorage.setItem("pcScore",pcScore);
-	}
-	console.log(pcScore);
+	if (!get("pcScore")) { pcScore === 0; set("pcScore", pcScore); } 
+	else { get("pcScore"); pcScore = isNaN(pcScore) ? 0 : pcScore; }
 
-	if ((myScore == 0) && (pcScore == 0)) { round = 1; 
-	} else { round = Number(myScore + pcScore) + 1; }
-	
+	// prevent ongoing rounds after win
+	if ((myScore >= 3) || (pcScore >= 3)) { window.location.reload(); } 
+}  // end tally
+
+function track() {
+	// count rounds
+	round === (myScore + pcScore); 
+	round = isNaN(round) ? 0 : round;
+	if ((round === 0) || (round < 5)) { round++; }
+	else { window.location.reload(); }
 	console.log(round);
-}
 
-// player wins
-function myWin() { 
-	tally();
-	myScore += 1;  
-	sessionStorage.setItem("myScore", myScore);
-	document.getElementById("myWins").innerHTML = myScore;
-	document.getElementById("pcWins").innerHTML = pcScore;
-
-	// return results
-	if ((myScore === 3) && (!pcScore)) { result = "welcome to the <br> HALL of FAME <br> please enter your initials below"; gameover(); }
-	else if ((myScore === 3) && (pcScore <= 2)) { result = "winner winner chicken dinner! <br> congratulations!!!"; gameover(); }
-	else { result = "YOU won round " + round + "! whoohoo! "; } 
-	console.log(myScore, round);
-} 
-
-// pc wins
-function pcWin() {
-	tally();
-	pcScore += 1; 
-	sessionStorage.setItem("pcScore", pcScore); 
-	document.getElementById("pcWins").innerHTML = pcScore;
-	document.getElementById("myWins").innerHTML = myScore;
-	
-	// return results
-	if ((pcScore === 3) && (!myScore)) { result = "dude, you need more practice... try again"; gameover(); }
-	else if ((pcScore === 3) && (myScore <= 2)) { result = "good try! <br> computer wins game"; gameover(); }
-	else { result = "PC wins round " + round + "! "; }
-	console.log(pcScore, round);
-}
+	// HTML output
+	$("myWins").innerHTML = myScore;
+	$("pcWins").innerHTML = pcScore;
+	$("toss").innerHTML = msg;
+	$("winner").innerHTML = result;
+}  // end track rounds
 
 function gameover() {
-	msg = 'thanks for playing! <br> <span onclick="window.location.reload();"> click here to play again</span>';
-}
-// eof -- end game
+	// HTML output
+	$("winner").innerHTML = result; 
+	result += '<span onclick="window.location.reload();"> <br > thanks for playing! <br> start a new game </span>';
+} // end gameover
+// eof 
